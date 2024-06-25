a6b51a032913332ceae24463bd123d0f068d36d6
Ryan Wynd <ryan.wynd@fdmgroup.com>
2024-06-25 14:16:44 +1000

    Changed to dotnet 8

--- RobotApp/RobotApp/Program.cs
+++ RobotApp/RobotApp/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RobotApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Test update");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
