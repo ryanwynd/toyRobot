747efa54c9dbb5b7405edc0ef1e40f9e81d5c60b
Ryan Wynd <ryan.wynd@fdmgroup.com>
2024-06-25 12:33:00 +1000

    testing hook

--- RobotApp/RobotApp/Program.cs
+++ RobotApp/RobotApp/Program.cs
@@ -10,6 +10,8 @@ namespace RobotApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Test update");
         }
     }
 }
