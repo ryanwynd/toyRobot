b308a911cb3e17dd7f3e35f5b58a53ed32cba10e
Ryan Wynd <ryan.wynd@fdmgroup.com>
2024-06-25 16:28:09 +1000

    Switched to blazor

--- RobotApp/RobotApp/Program.cs
+++ RobotApp/RobotApp/Program.cs
@@ -1,7 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-while (true)
-{
-    String userInput = Console.ReadLine();
-    Console.WriteLine("User said " + userInput);
-}
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RobotApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
