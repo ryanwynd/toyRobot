44e47b1c8a818c21b0f3ff21b11dbb4f079a9b61
Ryan Wynd <ryan.wynd@fdmgroup.com>
2024-06-25 14:25:42 +1000

    Test user unput on console

--- RobotApp/RobotApp/Program.cs
+++ RobotApp/RobotApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+while (true)
+{
+    String userInput = Console.ReadLine();
+    Console.WriteLine("User said " + userInput);
+}
