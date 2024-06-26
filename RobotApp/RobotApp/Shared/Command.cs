58fd62030d65a972ba1b39531cad4e9253a55b93
Ryan Wynd <ryan.wynd@fdmgroup.com>
2024-06-26 17:04:42 +1000

    Added simple whitespace check

---
+++ RobotApp/RobotApp/Shared/Command.cs
@@ -0,0 +1,14 @@
+﻿namespace RobotApp.Shared
+{
+    static class Command
+    {
+        public static bool ValidateCommand(string command)
+        {
+            if (!string.IsNullOrWhiteSpace(command))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
